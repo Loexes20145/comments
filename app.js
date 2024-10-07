f2c7118993ff7743e91ccd924904710f09e1dd63
loexes20145 <emaklov8145@gmail.com>
2024-10-07 15:19:34 +0000

    new commit

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-import { fetchJSON } from "./functions/api.js";
-import { cloneTemplate } from "./functions/dom.js";
+class InfinitePagination {
 
-try {
-    const comments = fetchJSON('https://jsonplaceholder.typicode.com/comments?_limit=10')
-    comments
-} catch (e) {
-    alert(e)
+    #endpoint
+    #template
+    #target
+    #elements
+
+    /**
+     * 
+     * @param {HTMLElement} element 
+     */
+    constructor (element) {
+        this.#endpoint = element.dataset.endpoint
+        this.#template = element.dataset.template
+        this.#target = element.dataset.target
+        this.#elements = element.dataset.elements
+    }
 }
-
-const target = document.querySelector('.spinner-border')
-
-/**
- * @returns {NodeList}
- */
-const loadMore = function () {
-    document.querySelector('.spinner-border').before(cloneTemplate('comment'))
-}
-
-const options = {
-    root : null,
-    rootMargin : "0px",
-    threshold: 1.0
-}
-
-const observer = new IntersectionObserver (loadMore, options)
-
-observer.observe(target)
--- functions/dom.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * @param {string} id 
- * @returns {DocumentFragment}
- */
-export function cloneTemplate (id) {
-    return document.getElementById(id).content.cloneNode(true)
-}
