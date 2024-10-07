f5be11c71be267d27c9d1003b267d90c006c1543
loexes20145 <emaklov8145@gmail.com>
2024-10-07 11:14:00 +0000

    new commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 import { fetchJSON } from "./functions/api.js";
+
+try {
+    const comments = fetchJSON('https://jsonplaceholder.typicode.com/comments?_limit=10')
+    comments
+} catch (e) {
+    alert(e)
+}
