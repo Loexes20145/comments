58452573d53daa5134521728101b8593c361e753
loexes20145 <emaklov8145@gmail.com>
2024-10-07 15:55:42 +0000

    new commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+import { fetchJSON } from "./function/api.js"
+
 class InfinitePagination {
 
+    /** @type {string} */
     #endpoint
+    /** @type {HTMLTemplateElement} */
     #template
+    /** @type {HTMLElement} */
     #target
+    /** @type {object} */
     #elements
+    /** @type {IntersectionObserver} */
+    #observer
+    /** @type {boolean} */
+    #loading = false
 
     /**
      * 
@@ -11,10 +21,34 @@ class InfinitePagination {
      */
     constructor (element) {
         this.#endpoint = element.dataset.endpoint
-        this.#template = element.dataset.template
-        this.#target = element.dataset.target
-        this.#elements = element.dataset.elements
-        console.log(this.#target)
+        this.#template = document.querySelector(element.dataset.template)
+        this.#target = document.querySelector(element.dataset.target)
+        this.#elements = JSON.parse(element.dataset.elements)
+        this.#observer = new IntersectionObserver((entries) => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    this.#loadMore()
+                }
+            }
+        })
+    this.#observer.observe(element)
+    }
+
+    async #loadMore() {
+        if (this.#loading) {
+            return
+        }
+        this.#loading = true
+        const comments = await fetchJSON(this.#endpoint)
+        for (const comment of comments) {
+            const commentElement= this.#template.content.cloneNode(true)
+            for (const [key, selector] of Object.entries(this.#elements)) {
+                commentElement.querySelector(selector).innerText = comment[key]
+                console.log({key, selector})
+            }
+            this.#target.append(commentElement)
+        }
+        this.#loading = false
     }
 }
 
