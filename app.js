33806e4659787c906fb3f05706ff8c0feb23cebd
loexes20145 <emaklov8145@gmail.com>
2024-10-07 16:05:35 +0000

    new commit

--- app.js
+++ app.js
@@ -8,18 +8,23 @@ class InfinitePagination {
     #template
     /** @type {HTMLElement} */
     #target
+    /** @type {HTMLElement} */
+    #loader
     /** @type {object} */
     #elements
     /** @type {IntersectionObserver} */
     #observer
     /** @type {boolean} */
     #loading = false
+    /** @type {number} */
+    #page = 1
 
     /**
      * 
      * @param {HTMLElement} element 
      */
     constructor (element) {
+        this.#loader = element
         this.#endpoint = element.dataset.endpoint
         this.#template = document.querySelector(element.dataset.template)
         this.#target = document.querySelector(element.dataset.target)
@@ -39,7 +44,15 @@ class InfinitePagination {
             return
         }
         this.#loading = true
-        const comments = await fetchJSON(this.#endpoint)
+        const url = new URL(this.#endpoint)
+        url.searchParams.set('_page', this.#page)
+
+        const comments = await fetchJSON(url.toString())
+        if (/*comments.length === 0*/ this.#page === 3) {
+            this.#observer.disconnect()
+            this.#loader.remove()
+            return
+        }
         for (const comment of comments) {
             const commentElement= this.#template.content.cloneNode(true)
             for (const [key, selector] of Object.entries(this.#elements)) {
@@ -48,6 +61,7 @@ class InfinitePagination {
             }
             this.#target.append(commentElement)
         }
+        this.#page++
         this.#loading = false
     }
 }
