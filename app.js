d6547a10ad593280d843117c77e611080d2a4b1c
loexes20145 <emaklov8145@gmail.com>
2024-10-07 12:05:54 +0000

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { fetchJSON } from "./functions/api.js";
+import { cloneTemplate } from "./functions/dom.js";
 
 try {
     const comments = fetchJSON('https://jsonplaceholder.typicode.com/comments?_limit=10')
@@ -6,3 +7,22 @@ try {
 } catch (e) {
     alert(e)
 }
+
+const target = document.querySelector('.spinner-border')
+
+/**
+ * @returns {NodeList}
+ */
+const loadMore = function () {
+    document.querySelector('.spinner-border').before(cloneTemplate('comment'))
+}
+
+const options = {
+    root : null,
+    rootMargin : "0px",
+    threshold: 1.0
+}
+
+const observer = new IntersectionObserver (loadMore, options)
+
+observer.observe(target)
